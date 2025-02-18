d08d0066465d1d28fe850a8bd9e74630aae46866
unknown <mystery832949@gmail.com>
2025-02-18 08:54:39 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'light-magenta': 'hsl(293, 100%, 63%)',
+        'Light-Violet' : 'hsl(264, 100%, 61%)',
+        'White': 'hsl(0, 0%, 100%)',
+         'Light-GrayishViolet' : 'hsl(270, 20%, 96%)',
+ 'Very-Dark-Desaturated-Violet'  : 'hsl(271, 36%, 24%)', 
+  'Very-Light-Magenta' : 'hsl(289, 100%, 72%)',
+  'Pale-Violet'  : 'hsl(276, 100%, 81%)',  
+ 'Moderate-Violet': 'hsl(276, 55%, 52%)', 
+  'Desaturated-Dark-Violet'  : 'hsl(271, 15%, 43%)',
+ 'Grayish-Blue': 'hsl(206, 6%, 79%)', 
+ 'Very-Dark-Desaturated-Violet':  'hsl(271, 36%, 24%)',
+ 'Dark-Grayish-Violet': 'hsl(270, 7%, 64%)'  
+      },
+
+     
+    },
+  },
+}
